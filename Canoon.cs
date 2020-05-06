73a5acea0e50d4db39759573cb63d8bc451b4c6f
mukhamad rizal <mukhamad.ilham@students.amikom.ac.id>
2020-05-06 22:02:57 +0700

    polymorphism

---
+++ Canoon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program
+{
+    public class Canoon : PrinterWindows
+    {
+        public override void Show()
+        {
+            Console.WriteLine("Canoon display dimension : 9.5*12 ");
+        }
+        public override void Print()
+        {
+            Console.WriteLine("Canoon Printer Printing....");
+        }
+    }
+}
---
+++ PrinterWindows.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program
+{
+    public class PrinterWindows
+    {
+        public string Name { get; set;}
+
+        public virtual void Show()
+        {
+            Console.WriteLine(" printer display dimension");
+        }
+        public virtual void Print()
+        {
+            Console.WriteLine("printer printing . . .");
+        }
+    }
+}
